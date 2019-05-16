b6d0e2213e687e69de5b08f6be42e8fa6fb3c763
askoenig <askoenig94@gmail.com>
2019-05-16 18:11:25 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,60 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all #define instance variable for view
+    erb :'figures/index' #show all figures view (index)
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+      # binding.pry
+    erb :'figures/new' #show new figures view
+  end
+
+  post '/figures' do
+    #below works with properly formatted params in HTML form
+    @figure = Figure.new(params[:figure]) #create new figure
+        # if params[figure.title_name] == nil && params[figure.landmark_name == nil]
+    if @figure.save #saves new figure or returns false if unsuccessful
+      redirect '/figures' #redirect back to figures index page
+    else
+      erb :'figures/new' # show new figures view again(potentially displaying errors)
+    end
+  end
+
+  get '/figures/:id' do
+    #gets params from url
+    @figure = Figure.find(params[:id]) #define instance variable for view
+    erb :'figures/show' #show single figure view
+  end
+
+  get '/figures/:id/edit' do
+    #get params from url
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find(params[:id]) #define intstance variable for view
+    erb :'figures/edit' #show edit figure view
+  end
+
+  put '/figures/:id' do
+
+  #get params from url
+  @figure = Figure.find(params[:id]) #define variable to edit
+  @figure.assign_attributes(params[:figure]) #assign new attributes
+  if @figure.save #saves new figure or returns false if unsuccessful
+    redirect '/figures' #redirect back to figures index page
+  else
+    erb :'figures/edit' #show edit figure view again(potentially displaying errors)
+  end
+end
+
+delete '/figures/:id' do
+#get params from url
+@figure = Figure.find(params[:id]) #define figure to delete
+@figure.destroy #delete figure
+redirect '/figures' #redirect back to figures index page
+end
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,54 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+  @landmark = Landmark.all #define instance variable for view
+  erb :'landmarks/index' #show all landmark view (index)
+end
+
+get '/landmarks/new' do
+  @figures = Figure.all
+  erb :'landmarks/new' #show new landmarks view
+end
+
+post '/landmarks' do
+  #below works with properly formatted params in HTML form
+  @landmark = Landmark.create(params[:landmark]) #create new landmark
+  if @landmark.save #saves new landmark or returns false if unsuccessful
+    redirect '/landmarks' #redirect back to landmarks index page
+  else
+    erb :'landmarks/new' # show new landmarks view again(potentially displaying errors)
+  end
+end
+
+get '/landmarks/:id' do
+  #gets params from url
+  @landmark = Landmark.find(params[:id]) #define instance variable for view
+  erb :'landmarks/show' #show single landmark view
+end
+
+get '/landmarks/:id/edit' do
+  #get params from url
+  @figures = Figure.all
+  @landmark = Landmark.find(params[:id]) #define intstance variable for view
+  erb :'landmarks/edit' #show edit landmark view
+end
+
+put '/landmarks/:id' do
+  #get params from url
+  @landmark = Landmark.find(params[:id]) #define variable to edit
+  @landmark.assign_attributes(params[:landmark]) #assign new attributes
+  if @landmark.save #saves new landmark or returns false if unsuccessful
+    redirect '/landmarks' #redirect back to landmarks index page
+  else
+    erb :'landmarks/edit' #show edit landmark view again(potentially displaying errors)
+  end
+end
+
+delete '/landmarks/:id' do
+  #get params from url
+  @landmark = Landmark.find(params[:id]) #define landmark to delete
+  @landmark.destroy #delete landmark
+  redirect '/landmarks' #redirect back to landmarks index page
+end
+
 end
