145e9baeac37978ba7efc5901f25d9e940688669
Chloe <gingkolane@gmail.com>
2019-05-19 11:35:40 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,55 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  # show all the figures
+  get '/figures' do
+    @figures = Figure.all
+  
+    erb :'figures/index'
+  end
+
+  # show a blank form for new figure
+  get '/figures/new' do
+    @figure = Figure.new
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new' 
+  end
+
+  # show a single figure page
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @landmarks = @figure.landmarks
+    @titles = @figure.titles
+    erb :'figures/show'
+  end
+
+  # create and post new figure data
+  post '/figures' do
+    @figure = Figure.create(params[:figure])  # must use params[:figure] if use _form.erb partial
+    redirect to "figures/#{@figure.id}"
+  end
+
+  # show a figure data on an edit page
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id]) #get params from url and define intstance variable for view
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/edit'   #show edit figure view
+  end
+
+  # Update a figure data
+  put '/figures/:id' do
+    #get params from url
+    @figure = Figure.find(params[:id]) #define variable to edit
+    @figure.update(params[:figure]) #assign new attributes to found id, can use assign_attributes
+    redirect "figures/#{@figure.id}"
+  end
+
+  delete '/figures/:id' do
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect 'students'
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,58 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  
+  # show all landmarks
+  get '/landmarks' do
+    @landmarks = Landmark.all #define instance variable for view
+    erb :'landmarks/index' #show all landmarks view (index)
+  end
+
+  # provide a blank form
+  get '/landmarks/new' do
+    @landmark = Landmark.new
+    erb :'landmarks/new' #show bank landmark form
+  end
+
+  # create a new landmark, store it in database, redirect to singe page view
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark]) #create new landmark
+    redirect to "/landmarks/#{@landmark.id}" #redirect back to landmarks index page
+  end
+
+  # display a single landmark by id
+  get '/landmarks/:id' do
+    #gets params from url
+    @landmark = Landmark.find(params[:id]) #define instance variable for view
+    erb :'landmarks/show' #show single landmark view
+  end
+
+  # show a single landmark in edit view
+  get '/landmarks/:id/edit' do
+
+    @landmark = Landmark.find(params[:id]) #define intstance variable for view
+    erb :'landmarks/edit' #show edit landmark view
+  end
+
+
+  # store edited data in database, page direct to single page view
+  put '/landmarks/:id' do  
+    @landmark = Landmark.find(params[:id]) #define variable to edit'
+    @landmark.update(params[:landmark]) #assign new attributes
+    
+    redirect to "/landmarks/#{@landmark.id}"
+
+    # erb :"landmarks/show"
+  end
+
+  delete '/landmarks/:id' do
+
+    #get params from url
+    @landmark = Landmark.find(params[:id]) #define landmark to delete
+  
+    @landmark.destroy #delete landmark
+  
+    redirect '/landmarks' #redirect back to landmarks index page
+  
+  end
+
 end
